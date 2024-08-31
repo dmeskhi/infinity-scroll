8f9083ad23651a71a11c6e838f80f1fb94329194
David Meskhishvili <dmeskh@gmail.com>
2024-08-31 12:52:25 -0700

    Finish the project

--- main.js
+++ main.js
@@ -1,12 +1,60 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photoArray = [];
 
 //Unsplash API
-const count = 10;
+const count = 5;
 const apiKey = 'gcO6HEgS6wU83Am1meK18CDDyhuVNwflNZ80S0BZcRA';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        count = 30;
+    } 
+}
+
+// Helper function to set Attributes on DOM elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Create elements for links & photos, add to DOM
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photoArray.length;
+    console.log('total images', totalImages);
+    photoArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // Load event
+        img.addEventListener('load', imageLoaded);
+
+        // Put <img> inside <a>, then put both inside imageContainer
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 // Get photos
 async function getPhotos() {
@@ -18,6 +66,14 @@ async function getPhotos() {
         // Catch error here
     }
 }
+
+// Load more photos with scroll event
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // Run function on page load
 getPhotos();
